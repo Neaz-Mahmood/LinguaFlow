a5b50fa740c1cfb8cefa549c72edd792ca5e76bb
neaz <neaz.mahmood@vivasoftltd.com>
2026-07-14 13:27:59 +0600

    fix git issue

--- backend/dist/modules/simulators/simulators.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { EvaluateShadowingDto, QuickOutputReplyDto } from './dto/simulators.dto';
-import { SimulatorsService } from './simulators.service';
-export declare class SimulatorsController {
-    private readonly simulatorsService;
-    constructor(simulatorsService: SimulatorsService);
-    evaluateShadowing(body: EvaluateShadowingDto): Promise<any>;
-    quickOutputReply(body: QuickOutputReplyDto): Promise<any>;
-}
--- backend/dist/modules/stories/stories.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '../../entities/user.entity';
-import { StoriesService } from './stories.service';
-export declare class StoriesController {
-    private readonly storiesService;
-    constructor(storiesService: StoriesService);
-    getStories(user: User): Promise<any[]>;
-}
